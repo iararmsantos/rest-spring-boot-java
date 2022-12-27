9123a1ab84606ec3213d53cdb7ee8facf1a40ead
IARA REGINA MARQUES DOS SANTOS <iararmsantos@gmail.com>
2022-12-27 09:40:47 -0500

    Simple Math Operations Added

---
+++ 6_ParamAndExceptions/src/main/java/com/iarasantos/controllers/MathController.java
@@ -0,0 +1,62 @@
+package com.iarasantos.controllers;
+
+import com.iarasantos.converters.NumberConverter;
+import com.iarasantos.exceptions.UnsupportedMathOperationException;
+import com.iarasantos.math.SimpleMath;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+    SimpleMath simpleMath = new SimpleMath();
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        return simpleMath.sum(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return simpleMath.subtraction(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value="/multiplication/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return simpleMath.multiplication(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value="/division/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return simpleMath.division(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value="/mean/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return simpleMath.mean(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value="/squareRoot/{number}", method=RequestMethod.GET)
+    public Double squareRoot(@PathVariable("number") String number) throws Exception {
+        if (!NumberConverter.isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return simpleMath.squareRoot(NumberConverter.covertToDouble(number));
+    }
+
+
+}
+
